e5c879e2a126a313cf3eb23b5367c6e7d5f1bd5b
westgv <gustavowestermann@gmail.com>
2025-04-30 20:37:39 -0300

    NavBar working + Starting Containers

--- LPRMonitor_App/src/App.tsx
+++ LPRMonitor_App/src/App.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import NavBar from './components/NavBar';
+import Camera from './components/containers/Camera';
 
 
 const App: React.FC = () => {
   return (
     <>
+    <div className="flex h-screen">
       <NavBar />
+      <div className="flex-1 m-6 bg-black relative border-4 border-blue-600">
+        <div className="flex justify-between">
+          <Camera />
+          <div className="bg-white text-[#0000] h-1/2">SALVE</div>
+
+        </div>
+      </div>
+    </div>
     </>
   );
 };
---
+++ LPRMonitor_App/src/components/containers/Camera.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+interface CameraProps {
+    imageSrc?: string;
+    altText?: string;
+}
+
+const Camera: React.FC<CameraProps> = ({ imageSrc, altText = 'Camera feed' }) => {
+    return (
+        <div className="flex-none w-1/2 h-1/2 bg-white flex justify-center items-center">
+            {imageSrc ? (
+                <img src={imageSrc} alt={altText} className="max-w-full max-h-full" />
+            ) : (
+                <p className="text-[16px] text-[#888]">No image available</p>
+            )}
+        </div>
+    );
+};
+
+export default Camera;
